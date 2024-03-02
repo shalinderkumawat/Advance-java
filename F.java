edee436b9a4813436102fd72167e4b0224a182ac
shalinder kumawat <kumarkumawatkumarkumawat@gmail.com>
2024-03-02 15:18:36 +0530

    frist new bassic file

---
+++ F.java
@@ -0,0 +1,138 @@
+Q1.
+   a-b;
+
+import java.util.*;
+public class F
+{
+    public static void main(String[] args)
+{
+    Scanner sc=new Scanner (System.in);
+    System.out.println("enter the value of number 1 :");
+    int a=sc.nextInt();
+
+    System.out.println("enter the value of b number :");
+    int b=sc.nextInt();
+    System.out.println("a-b ="+(a-b) );
+   
+}
+
+Q2.
+a*b;
+import java.util.*;
+public class F
+{
+    public static void main(String[] args)
+{
+    Scanner sc=new Scanner (System.in);
+    System.out.println("enter the value of number 1 :");
+    int a=sc.nextInt();
+
+    System.out.println("enter the value of b number :");
+    int b=sc.nextInt();
+    System.out.println("a-b ="+(a*b) );
+   
+}
+
+Q3. 
+   a/b;
+
+import java.util.*;
+public class F
+{
+    public static void main(String[] args)
+{
+    Scanner sc=new Scanner (System.in);
+    System.out.println("enter the value of number 1 :");
+    int a=sc.nextInt();
+
+    System.out.println("enter the value of b number :");
+    int b=sc.nextInt();
+    System.out.println("a-b ="+(a/b) );
+   
+}
+
+Q4.  a+b+c+d
+
+
+import java.util.*;
+public class F{
+    public static void main(String[] args)
+    {
+     
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the frist number :");
+        int a=sc.nextInt();
+        System.out.println("ente the secoind number :");
+        int b=sc.nextInt();
+        System.out.println("enter the threed number of value :");
+        int c=sc.nextInt();
+        System.out.println("enter the forth number");
+        int d=sc.nextInt();
+        System.out.println(a+"+"+b+"+"+c+"+"+d+"="+ (a+b+c+d) );
+    }
+}
+
+Q5.a+b-c*d
+
+
+   import java.util.*;
+public class F{
+    public static void main(String[] args)
+    {
+     
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the frist number :");
+        int a=sc.nextInt();
+        System.out.println("ente the secoind number :");
+        int b=sc.nextInt();
+        System.out.println("enter the threed number of value :");
+        int c=sc.nextInt();
+        System.out.println("enter the forth number");
+        int d=sc.nextInt();
+        System.out.println(a+"+"+b+"-"+c+"*"+d+"="+ (a+b-c*d) );
+    }
+}
+
+Q6.
+    import java.util.*;
+public class F{
+    public static void main(String[] args)
+    {
+     
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the frist number :");
+        int a=sc.nextInt();
+        System.out.println("ente the secoind number :");
+        int b=sc.nextInt();
+        System.out.println("enter the threed number of value :");
+        int c=sc.nextInt();
+        System.out.println("enter the forth number");
+        int d=sc.nextInt();
+        System.out.println(a+"+"+b+"-"+c+"*"+d+"="+ (a+(b*c)-d) );
+    }
+}
+
+Q7.
+
+(a+(b*c)-d);
+
+
+import java.util.*;
+public class F{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner (System.in);
+        System.out.println("enter the value of float value of one :");
+        float a=sc.nextFloat();
+        System.out.println("enter the secoind value float value of :");
+        float b=sc.nextFloat();
+        System.out.println("enter the threed float value of :");
+        float c=sc.nextFloat();
+        System.out.println("enter the value of forth number of :");
+        int d=sc.nextInt();
+        System.out.println(a+"+"+b+"*"+c+"+"+d+"="+ (a+(b*c)-d));
+    }
+}
+ 
+Q8.
+   
