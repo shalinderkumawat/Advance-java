af793e0018777207795b6e72a9a46a370b14a31e
shalinder kumawat <kumarkumawatkumarkumawat@gmail.com>
2024-04-27 16:42:03 +0530

    done

--- All.java
+++ All.java
@@ -1,32 +1,57 @@
-class A extends Thread{
-    public void run(){
-        for(int i=1;i<=5;i++)
+public class Main{
+    
+    
+    
+    static void swap(int a[],int i,int j)
+    {
+        int temp=i;
+        i=j;
+        j=temp;
+    }
+    
+    static void print(int a[])
+    {
+        int b[]=new int[a.length];
+        int c[]=new int[a.length];
+        
+       for(int i=0;i<=a.length-1;i++)
         {
-            try{
-                Thread.sleep(900);
-            }
-            catch(Exception ex)
+            if(i%2==0)
             {
-
+                b[i]=a[i];
             }
-            System.out.print(i+" ");
+           
+        }
+        
+        for(int i=0;i<=a.length-1;i++)
+        {
+          if(i%2!=0)
+          {
+              c[i]=i;
+          }
+           
+        } 
+        
+        for(int i=0;i<=a.length-1;i++)
+        {
+            for(int j=0;j<=a.length-1;i++)
+            {
+                if(b[i]>c[j])
+                {
+                   swap(a, i, j);
+                }
+            }
+        }
+        for(int i=0;i<=a.length-1;i++)
+        {
+            System.out.print(a[i]+" ");
         }
     }
-}
-public class All{
     public static void main(String [] args)
     {
-        A sc=new A();
-        A cs=new A();
-
-        sc.start();
-        try{
-            sc.join();
-        }
-        catch(Exception k)
-        {
-
-        }
-        cs.start();
+        int a[]={1,2,4,5,6};
+        print(a);
+       
+       
     }
 }
