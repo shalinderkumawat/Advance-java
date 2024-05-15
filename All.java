9e3d1976db68e90561b1d162f81061ec505f15a7
shalinder kumawat <kumarkumawatkumarkumawat@gmail.com>
2024-05-15 21:50:31 +0530

    done work

--- All.java
+++ All.java
@@ -1,57 +1,6 @@
-public class Main{
-    
-    
-    
-    static void swap(int a[],int i,int j)
-    {
-        int temp=i;
-        i=j;
-        j=temp;
-    }
-    
-    static void print(int a[])
-    {
-        int b[]=new int[a.length];
-        int c[]=new int[a.length];
-        
-       for(int i=0;i<=a.length-1;i++)
-        {
-            if(i%2==0)
-            {
-                b[i]=a[i];
-            }
-           
-        }
-        
-        for(int i=0;i<=a.length-1;i++)
-        {
-          if(i%2!=0)
-          {
-              c[i]=i;
-          }
-           
-        } 
-        
-        for(int i=0;i<=a.length-1;i++)
-        {
-            for(int j=0;j<=a.length-1;i++)
-            {
-                if(b[i]>c[j])
-                {
-                   swap(a, i, j);
-                }
-            }
-        }
-        for(int i=0;i<=a.length-1;i++)
-        {
-            System.out.print(a[i]+" ");
-        }
-    }
+public class All{
     public static void main(String [] args)
     {
-        int a[]={1,2,4,5,6};
-        print(a);
-       
-       
+        sou 
     }
 }
