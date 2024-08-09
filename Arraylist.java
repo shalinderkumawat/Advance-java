8f28d3c12d3964b6afdf452a63df05cf7008d5fc
shalinder kumawat <kumarkumawatkumarkumawat@gmail.com>
2024-08-09 22:02:30 +0530

    even number in Arraylist

--- Arraylist.java
+++ Arraylist.java
@@ -21,3 +21,22 @@ public class Arraylist {
     }
 }
 
+//  This is even number in Arraylist
+
+public class Main{
+    public static void main(String [] args)
+    {
+        ArrayList<Integer> sc=new ArrayList<>();
+        for(int i=1;i<=5;i++)
+        {
+            sc.add(i);
+        }
+        for(int i=0;i<sc.size();i++)
+        {
+            if(sc.get(i)%2==0)
+            {
+                System.out.println("This is even number : "+sc.get(i));
+            }
+        }
+    }
+}
